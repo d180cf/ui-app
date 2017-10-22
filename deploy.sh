e89b0603bca7fcc65e0de40b7c99e5e4fb1781ce
d180cf <d180cf@gmail.com>
2017-10-22 00:55:59 -0700

    Moved from the tsumego.js repo

---
+++ deploy.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# This script is run by Travis CI after the build succeeds.
+# It copies the .bin folder and sends it to d180cf/d180cf.github.io repo
+# in a commit with a message like "Build 123 (d180cf/ui-app master 1ef8c97)"
+
+set -e # exit with nonzero exit code if anything fails
+
+if [[ $TRAVIS_BRANCH == "master" && $TRAVIS_PULL_REQUEST == "false" ]]; then
+
+echo "Starting to update ${GH_REPO}"
+
+#copy data we're interested in to other place
+cp -R .bin $HOME/site
+
+#go to home and setup git
+cd $HOME
+git config --global user.email "d180cf@gmail.com"
+git config --global user.name "d180cf (via Travis CI)"
+
+echo "Cloning the repo..."
+git clone https://${GH_TOKEN}@github.com/${GH_USER}/${GH_REPO} repo
+
+echo "Copying the site files..."
+cd repo
+cp -Rf $HOME/site/* .
+
+echo "Creating a new commit..."
+git add -f .
+git commit -m "Build $TRAVIS_BUILD_NUMBER ($TRAVIS_REPO_SLUG $TRAVIS_BRANCH $TRAVIS_COMMIT)"
+
+echo "Pushing changes..."
+git push
+
+echo "Done updating ${GH_REPO}"
+
+else
+ echo "Skipped updating ${GH_REPO}, because build is not triggered from the master branch."
+fi;
---
+++ jakefile.js
@@ -0,0 +1,99 @@
+/* global jake:false */
+/* global task:false */
+/* global desc:false */
+/* global complete:false */
+/* global process:false */
+
+var exec = (cmd, arg) => new Promise(resolve => {
+    jake.exec(cmd, arg || {}, resolve);
+});
+
+function babel(args) {
+    args = args || {};
+
+    var fs = require('fs');
+    var path = require('path');
+    var babel = require('babel');
+
+    var tsconfig = require(path.resolve('tsconfig.json'));
+
+    var sourceMaps = args.sourceMaps === undefined ? true : !!args.sourceMaps;
+    var srcPath = args.srcPath || tsconfig.compilerOptions.out;
+    var outPath = args.outPath || srcPath;
+
+    var es6src = fs.readFileSync(srcPath, 'utf8');
+    var es6map = sourceMaps && fs.readFileSync(srcPath + '.map', 'utf8');
+
+    var es5 = babel.transform(es6src, {
+        inputSourceMap: sourceMaps && JSON.parse(es6map),
+        sourceMaps: sourceMaps,
+        loose: args.loose === false ? undefined : 'all',
+        compact: false,
+        blacklist: args.blacklist || [
+            'regenerator',
+            //'es6.forOf',
+            //'es6.classes',
+            //'es6.blockScoping',
+            //'es6.templateLiterals',
+        ]
+    });
+
+    es5.code = es5.code.replace(
+        /(\r?\n\/\/# sourceMappingURL=)(.+)$/img,
+        sourceMaps ? '$1' + outPath + '.map' : '$1(removed; see jakefile.js)');
+
+    fs.writeFileSync(outPath, es5.code, 'utf8');
+    sourceMaps && fs.writeFileSync(outPath + '.map', JSON.stringify(es5.map, null, '\t'), 'utf8');
+}
+
+desc('Builds the testbench app.');
+task('tb', { async: true }, mode => {
+    console.log('building the testbench app...');
+
+    exec('node node_modules/typescript/lib/tsc', { printStdout: true }).then(() => {
+        if (mode == 'dev') {
+            // TODO: babel screws source maps
+            console.log('skipping babel in the dev mode...');
+        } else {
+            return babel({
+                loose: false,
+                blacklist: [
+                    //'regenerator', // doesn't work in IE/Edge
+                    //'es6.forOf', // doesn't work in IE/Edge
+                    //'es6.blockScoping', // otheriwse const/let variables won't be bound in loops with callbacks
+                ]
+            });
+        }
+    }).then(() => {
+        console.log('rebuilding problems/manifest.json...');
+        process.chdir('node_modules/problems');
+        return exec('npm i');
+    }).then(() => {
+        return exec('node sgf > manifest.json');
+    }).then(() => {
+        process.chdir('../..');
+    }).then(complete);
+});
+
+desc('Builds the site contents.');
+task('site', ['tb'], () => {
+    console.log('building the site...');
+    jake.rmRf('.bin');
+    jake.mkdirP('.bin');
+    jake.cpR('libs', '.bin');
+    jake.cpR('node_modules/problems', '.bin/problems');
+    jake.rmRf('.bin/problems/node_modules');
+
+    jake.cpR('node_modules/tsumego.js/bin/tsumego.js', '.bin');
+    jake.cpR('node_modules/tsumego.js/bin/tsumego.es5.js', '.bin');
+    jake.cpR('node_modules/tsumego.js/bin/tsumego.es6.js', '.bin');
+
+    jake.cpR('index.html', '.bin');
+    jake.cpR('favicon.ico', '.bin');
+    jake.cpR('styles', '.bin');
+});
+
+desc('Builds everything.');
+task('default', ['tb'], () => {
+    console.log('done!');
+});
